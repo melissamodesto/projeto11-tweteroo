20f8aa39a764c90138489b9e7cffe1887b65e877
melissamodesto <mbm.modesto@gmail.com>
2022-08-29 15:30:29 -0300

    Bonus Data validation, status code, tweets posted by route parameters and paging

--- src/index.js
+++ src/index.js
@@ -8,56 +8,73 @@ let tweets = [];
 
 //Criando o servidor
 const server = express();
-server.use(json()); 
-server.use(cors()); 
+server.use(json());
+server.use(cors());
 
 //Criando rotas
 server.post('/sign-up', (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    if (user.username.length !== 0 && user.avatar.length !== 0) {
-        users.push(user);
-        return res.status(201).send('OK');
-    } else {
-        return res.status(400).send('Preencha todos os campos');    
-    }
-    
-})
+  if (user.username.length !== 0 && user.avatar.length !== 0) {
+    users.push(user);
+    return res.status(201).send('OK');
+  } else {
+    return res.status(400).send('Preencha todos os campos');
+  }
+});
 
 server.post('/tweets', (req, res) => {
-    const tweet = {
-        username: req.body.username,
-        tweet: req.body.tweet,
-        avatar: users.find((user) => user.username === req.body.username).avatar,
-    }
+  const body = req.body;
 
-    if (tweet.username.length !== 0 && tweet.tweet.length !== 0) {
-        tweets.push(tweet);
-        return res.status(201).send('OK');
-    } else {
-        return res.status(400).send('Preencha todos os campos');    
-    }
-    
-})
+  if (!body.tweet) {
+    res.status(400).send('Todos os campos são obrigatórios!');
+  } else {
+    const user = users.find((user) => user.username === req.header('User'));
+    const newTweet = {
+      username: user.username,
+      avatar: user.avatar,
+      tweet: body.tweet,
+    };
+    tweets.push(newTweet);
+    res.status(201).send('Ok');
+  }
+});
 
 server.get('/tweets', (req, res) => {
+  const page = parseInt(req.query.page);
+  if (page >= 1) {
+    res.send(
+      tweets
+        .slice(
+          Math.max(tweets.length - 10 * page, 0),
+          Math.max(tweets.length - 10 * (page - 1), 0)
+        )
+        .map((tweet) => {
+          return {
+            ...tweet,
+            avatar: users.find((user) => user.username === tweet.username)
+              .avatar,
+          };
+        })
+        .reverse()
+    );
+  } else {
+    res.status(400).send('Informe uma página válida!');
+  }
+});
 
-    let lastTenTweets = [];
+server.get('/tweets/:username', (req, res) => {
+  const { username } = req.params;
+  const user = users.find((user) => user.username === username);
 
-    for (let i = tweets.length - 1; i > tweets.length - 11; i--) {
-        const avatarUser = users.find(user => tweets[i].username === user.username).avatar;
-        lastTenTweets = [...lastTenTweets, {
-            username: tweets[i].username,
-            avatar: avatarUser,
-            tweet: tweets[i].tweet
-        }]
-    }
+  if (!user) {
+    return res.status(404).send('Usuário não encontrado');
+  }
 
-    res.send(lastTenTweets);
-})
+  const tweetsUser = tweets.filter((tweet) => tweet.username === username);
+
+  res.send(tweetsUser);
+});
 
 //Iniciando o servidor
-server.listen(5000, () => {
-    console.log(chalk.green('Online Server'));
-})
-
+server.listen(5000, () => console.log(chalk.green('Online Server')));
